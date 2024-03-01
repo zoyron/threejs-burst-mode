28df20f3d829298e71674c19402ebdfff874bd91
zoyron <sgrarora3@gmail.com>
2024-03-02 04:29:08 +0530

    added the basic canvas and scene

---
+++ src/script.js
@@ -0,0 +1,11 @@
+improt * as THREE from 'three';
+
+// making a canvas
+const canvas = document.querySelector('canvas.webgl'); // here we made an empty canvas
+
+// now we must initiate a scene to the canvas that we just created
+// scene
+const scene = new THREE.Scene();
+
+
+
