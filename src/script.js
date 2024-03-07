31588167c6294461d33ab4c5e821cf58c8cb31c4
zoyron <sgrarora3@gmail.com>
2024-03-07 18:52:00 +0530

    added the style sheet

--- src/script.js
+++ src/script.js
@@ -1,80 +1,71 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-
-// cursor - getting the co-ordinates of the mouse
-
-const cursor = {
-  x:0,
-  y:0
-};
-window.addEventListener('mousemove', (event) => {
-  cursor.x = event.clientX / sizes.width - 0.5; 
- // (event.clientX/sizes.width) this value would range from 0-1, so by subtracting 0.5 from it, we are changing the range to [-0.5, 0.5]
-  cursor.y = event.clientY / sizes.height - 0.5;
-});
-
-
-
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl');
-
-// Sizes
-const sizes = {
-    width: 800,
-    height: 600
-}
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
-// Object
-const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-scene.add(mesh);
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({color: 'red'})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-//camera.position.x = 2;
-//camera.position.y = 2;
-camera.position.z = 3;
-camera.lookAt(mesh.position);
-scene.add(camera);
-
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-// Renderer
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-});
-renderer.setSize(sizes.width, sizes.height);
-
-// Animate
-const clock = new THREE.Clock();
-
-const tick = () =>{
-    const elapsedTime = clock.getElapsedTime();
-
-    // Update objects
-    //mesh.rotation.y = elapsedTime;
-    
-    // update camera using mouse
-    //camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-    //camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-    //camera.position.y = cursor.y * 5;
-    //camera.lookAt(mesh.position);
-  // update controls
-    controls.update();
-    // Render
-    renderer.render(scene, camera);
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick);
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight 
 }
 
-tick();
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// since higher pixel ratios can consume a lot of resources and having pixel ratio of 2 is a very good thing, so we're limiting pixel
+// ratios of devices such as mobile phones/tablets that have pixel ratios as high as 5 to the pixel ratio of 2
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
