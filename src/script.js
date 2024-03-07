dcbc5a28eac6e207eaf17fc07e2a7b9d4d3a060a
zoyron <sgrarora3@gmail.com>
2024-03-07 11:38:39 +0530

    added three co-ordinates

--- src/script.js
+++ src/script.js
@@ -17,6 +17,16 @@ const meshMaterial = new THREE.MeshBasicMaterial({color:'red'});
 // now that we have created the cover material and the geometry, let's join them to make the required object or mesh
 const cubeMesh = new THREE.Mesh(redCubeGeometry, meshMaterial); // this is the cube object
 scene.add(cubeMesh); // added our object to the scene
+cubeMesh.position.x = 0.7; // here 1 could be of any unit depending upon the project you're making
+cubeMesh.position.y = -0.6;
+cubeMesh.position.z = 1;
+// let's say you're making a house project and you decided the units should be foot, just decided it and stick to it, no need to specify
+// in the code, and let's say you're building a landscape, you can just decide the units would ke in Kilometers. The catch here is to
+// stick to it. So if you decided that units are in meters, and you need to use somehing in 1Km, then put value as 1000 because you have 
+// stick to the unit you're decided for yourself in your mind
+
+
+
 
 const sizes ={
   width: 800,
@@ -38,5 +48,27 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
