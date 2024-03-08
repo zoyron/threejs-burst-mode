cc4cfeca7a7b317b2eaf4758da93454ba946415b
zoyron <sgrarora3@gmail.com>
2024-03-08 11:23:49 +0530

    added wireframe and changed the color to cyan

--- src/script.js
+++ src/script.js
@@ -13,8 +13,11 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({color: 'red'})
+const geometry = new THREE.BoxGeometry(1, 1, 1,2,2,2);
+const material = new THREE.MeshBasicMaterial({
+  color: 'cyan',
+  wireframe: true,
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
