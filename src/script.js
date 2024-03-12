7abc08b859e98f369f0c6fef6f776bebe949c38b
zoyron <sgrarora3@gmail.com>
2024-03-12 18:15:41 +0530

    moved the old script file to texture notes and initiated the script, without the mesh this time

--- src/script.js
+++ src/script.js
@@ -1,55 +1,6 @@
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-import imageSource from "../static/textures/minecraft.png"
 
-/*
- * Textures
- */
-//{{ const image = new Image();
-//
-//// we can't use the image directly, first we have to convert it to a texture. It(Texture) is a class in threejs that converts an image
-//// to something more GPU friendly(i.e. a texture), also, enabling textures would give us move features
-//
-//const texture = new THREE.Texture(image);
-//texture.colorSpace = THREE.SRGBColorSpace;
-//// we use the texture on the material, so instead of a color we can also give a texture to cover the geometry with
-//
-//// when the image is being loaded, the function inside the object "image" will be triggered
-//image.onload = () => {
-//  this.exture.needsUpdate = true;
-//};
-//image.src = imageSource; }}
-
-/*the above was a long way using texture in threejs, now we will see a shorter way
- * we did the above method to know what goes behind the scene of texture loading
-*/
-const loadingManager = new THREE.LoadingManager();
-
-const textureLoader = new THREE.TextureLoader(loadingManager);
-loadingManager.onStart = () => {
-  console.log('onStart');
-}
-loadingManager.onLoad = () => {
-  console.log('onLoad');
-}
-loadingManager.onError = () => {
-  console.log('onError');
-}
-const colorTexture = textureLoader.load(imageSource);
-
-// one texture loader can load multiple textures
-colorTexture.colorSpace = THREE.SRGBColorSpace;
-// the above three lines are working just as fine to make texture as the big ass fat arrow function we made earlier
-
-// right now we are usign just one texture and one textureLoader, but later on we will be using multiple textures, models, fonts
-// to keep a track of all that we will need a loadingManager, so we will use one like below
-//colorTexture.repeat.x = 2; // this means how many times the image should be used as a texture on the geometry in x-direction 
-//colorTexture.repeat.y = 3; // this means how many times the image should be used as a texture on the geometry in y-direction 
-//// the 'repeat' method is of the type 'Vetor2'
-////the following 2 methods bring the upper 2 methods to frutation
-//colorTexture.wrapS = THREE.MirroredRepeatWrapping; 
-//colorTexture.wrapT = THREE.MirroredRepeatWrapping;
-colorTexture.magFilter = THREE.NearestFilter;
 /**
  * Base
  */
@@ -58,16 +9,6 @@ const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
-
-/**
- * Object
- */
-//const geometry = new THREE.BoxGeometry(0.45, 0.45, 0.45, 3, 3, 3);
-const geometry = new THREE.TorusGeometry(0.5,0.17,32,50);
-//const geometry = new THREE.SphereGeometry(0.5,32,32);
-const material = new THREE.MeshBasicMaterial({ map: colorTexture, wireframe: !false });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
 
 /**
  * Sizes
@@ -103,7 +44,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.x = 1;
 camera.position.y = 1;
-camera.position.z = 1;
+camera.position.z = 2;
 scene.add(camera);
 
 // Controls
