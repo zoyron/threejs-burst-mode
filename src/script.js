e9ed2d0ccccfc66f74704ce02e65dcfd9095af6f
zoyron <sgrarora3@gmail.com>
2024-03-13 19:33:08 +0530

    added the font and licence

--- src/script.js
+++ src/script.js
@@ -1,14 +1,117 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import GUI from 'lil-gui'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import GUI from "lil-gui";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js"
 
-// debug GUI
+/**
+ * Base
+ */
+// Debug
 const gui = new GUI();
+gui.close();
 
-// getting the canvas
-const canvas = document.querySelector('canvas.webgl');
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
 
-// adding the scene
+// Scene
 const scene = new THREE.Scene();
 
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+const oneTexture = textureLoader.load('./textures/matcaps/3.png' );
 
+/*
+ * Fonts
+*/
+// one fontLoader can load multiple fonts
+const fontLoader = new FontLoader();
+fontLoader.load(
+  './fonts/helvetiker_regular.typeface.json',
+  // this function will be triggered once the font is loaded
+  () => {
+    console.log("font loaded");
+  }
+);
+
+
+/**
+ * Object
+ */
+const sphereGeometry = new THREE.SphereGeometry(0.5, 16, 16);
+const material = new THREE.MeshBasicMaterial({ map: oneTexture });
+//material.wireframe = true;
+const sphere = new THREE.Mesh(sphereGeometry, material);
+scene.add(sphere);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100,
+);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  sphere.rotation.y = elapsedTime * 0.5;
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
